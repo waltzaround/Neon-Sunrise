22030029512963cd23ec085006d468f1b3284ac8
Walter Lim <waltissomewhere@gmail.com>
2019-07-20 18:59:34 +1200

    first ver

---
+++ Assets/EnemyDeath.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDeath : MonoBehaviour
+{
+
+    public GameObject enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Hit by laser");
+        if (collision.gameObject.tag == "bullet")
+        {
+            enemy.SetActive(false);
+            Debug.Log("Sucessfully triggered");
+        }
+    }
+}
