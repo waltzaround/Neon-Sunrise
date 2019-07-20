f6d16f08a779ca5ea82cbd176837adf8033ad6ed
Aidan Harris <aidanharris1001@gmail.com>
2019-07-20 13:17:13 +1200

    Added bullet firing scripts and object pooler

---
+++ Assets/Scripts/BulletBehaviour.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+  public GameObject bullet;
+
+  public float disableBulletDistance;
+  void Update() {
+    float distanceFromCamera = Vector3.Distance(bullet.transform.position, Camera.main.transform.position);
+    if(distanceFromCamera > disableBulletDistance) {
+      DisableBullet();
+    }
+  }
+  void OnCollisionEnter(Collision collision) 
+  {
+    DisableBullet();
+  }
+
+  void DisableBullet() {
+    bullet.SetActive(false);
+    bullet.GetComponent<Rigidbody>().velocity = Vector3.zero;
+  }
+}
---
+++ Assets/Scripts/BulletShooter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class BulletShooter : MonoBehaviour
+{
+  public ObjectPooler objectPooler;
+  public float speed = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+          GameObject shootingBullet = objectPooler.GetPooledObject();
+          shootingBullet.SetActive(true);
+          shootingBullet.transform.position = Camera.main.transform.position;
+          shootingBullet.transform.rotation = Camera.main.transform.rotation;
+          Rigidbody shootingBulletRigidbody = shootingBullet.GetComponent<Rigidbody>();
+          
+          shootingBulletRigidbody.AddForce(Camera.main.transform.forward * speed);
+        }
+    }
+}
