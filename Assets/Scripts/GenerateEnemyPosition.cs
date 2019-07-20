479ec61b80d529506c3cb3df82ca6e0b28367f02
Monique Warrington <niquewarrington@gmail.com>
2019-07-20 15:05:54 +1200

    EnemySpawner Script written. Spawns enemies at random locations every 3 seconds currently

---
+++ Assets/Scripts/GenerateEnemyPosition.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateEnemyPosition : MonoBehaviour
+{
+ public static Vector3 GenerateEnemyPositionMethod()
+ {
+   //Scale 10 if need bigger range
+   Vector3 enemyPosition = Random.onUnitSphere * (10);
+   enemyPosition.z = Mathf.Abs(enemyPosition.z);
+   Debug.Log(enemyPosition);
+   return enemyPosition ;
+ }
+}
