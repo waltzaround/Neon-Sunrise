370e2c65fa06bf2701d53729482531ff3eb55263
Monique Warrington <niquewarrington@gmail.com>
2019-07-21 16:51:46 +1200

    Reset level implemented

---
+++ Assets/Scripts/Congratulations.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Congratulations : MonoBehaviour
+{
+    public GameObject congratulations;
+    public float timeLeft = 120.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        congratulations.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft <= 0)
+        {
+            congratulations.SetActive(true);
+           if (Input.touchCount > 0 ){
+               congratulations.SetActive(false);
+           }
+        }
+    }
+}
--- Assets/Scripts/CountdownTimer.cs
+++ Assets/Scripts/CountdownTimer.cs
@@ -1,22 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CountdownTimer : MonoBehaviour
 {
     public float timeLeft = 120.0f;
     public GameObject textMesh;
-    //public Text countdownText; //used for countdown
 
     // Update is called once per frame
     void Update()
     {
         timeLeft -= Time.deltaTime;
         textMesh.GetComponent<TextMesh>().text = "Time Left:" + (timeLeft).ToString("0") + "s";
-        //countdownText.text = "Time Remaining: " + (timeLeft).ToString("0") + " 's";
-        if (timeLeft < 0)
+        if (timeLeft <= 0)
         {
-           Debug.Log("Game Over!!!!");
+            Time.timeScale = 0.0f;
+            // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            // Time.timeScale = 1.0f;
+           if (Input.touchCount > 0 ){
+               Time.timeScale = 1.0f;
+               SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+           }
         }
     }
 }
