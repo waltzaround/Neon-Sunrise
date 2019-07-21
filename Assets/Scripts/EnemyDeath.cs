bfc9b6527bcb0d1530d17e1e2c134bd40ccad432
Walter Lim <waltissomewhere@gmail.com>
2019-07-21 14:55:37 +1200

    no message

--- Assets/Scripts/EnemyDeath.cs
+++ Assets/Scripts/EnemyDeath.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EnemyDeath : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +14,7 @@ public class EnemyDeath : MonoBehaviour
     public void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "bullet")
-        {
+
             gameObject.SetActive(false);
             GlobalVariables.score += 5;
         }
