f7ee545b61937ddc0f894510987f06a0b2d6f75e
Walter Lim <waltissomewhere@gmail.com>
2019-07-21 15:06:52 +1200

    Revert "no message"
    This reverts commit 33b84ba9b7426d0ad0861b9d835e1277c8b79afc.

--- Assets/Scripts/EnemyDeath.cs
+++ Assets/Scripts/EnemyDeath.cs
@@ -4,18 +4,17 @@ using UnityEngine;
 
 public class EnemyDeath : MonoBehaviour
 {
- 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     public void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "bullet")
-        {
-           
+
             gameObject.SetActive(false);
             GlobalVariables.score += 5;
         }
