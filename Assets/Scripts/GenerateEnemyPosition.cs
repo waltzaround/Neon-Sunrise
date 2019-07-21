2cf9aba20555d0931026e783eea05b7460c3fdb0
Walter Lim <waltissomewhere@gmail.com>
2019-07-21 17:16:33 +1200

    final commit and build

--- Assets/Scripts/GenerateEnemyPosition.cs
+++ Assets/Scripts/GenerateEnemyPosition.cs
@@ -7,7 +7,7 @@ public class GenerateEnemyPosition : MonoBehaviour
  public static Vector3 GenerateEnemyPositionMethod()
  {
    //Scale 10 if need bigger range
-   Vector3 enemyPosition = Random.onUnitSphere * (30);
+   Vector3 enemyPosition = Random.onUnitSphere * (20);
    enemyPosition.y = Mathf.Abs(enemyPosition.y);
    return enemyPosition ;
  }
