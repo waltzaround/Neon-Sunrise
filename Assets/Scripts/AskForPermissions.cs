a438ad5384d2ab52368060911c65b181ee67ec89
John Cave <me@johncave.co.nz>
2019-08-11 06:34:17 +0000

    Added text outline to ui

---
+++ Assets/Scripts/AskForPermissions.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Android;
+
+
+public class MicrophoneTest : MonoBehaviour
+{
+    GameObject dialog = null;
+
+    void Start()
+    {
+
+        if (!Permission.HasUserAuthorizedPermission(Permission.Microphone))
+        {
+            Permission.RequestUserPermission(Permission.Microphone);
+
+            if (!Permission.HasUserAuthorizedPermission(Permission.Microphone))
+            {
+                // The user has not authorized use of the microphone.
+                Application.Quit();
+            }
+            dialog = new GameObject();
+        }
+
+    }
+
+    // Now you can do things with the microphone
+
+}
